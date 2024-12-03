73c88650b65a7e9ff3d28d567c2a6d7d93aaf1c1
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-12-03 13:51:29 +0530

    fixed workflows

--- packages/auth/src/components/Header.js
+++ packages/auth/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      Header
+      <div></div>
+      <div>
+        <button>login</button>
+        <button>logout</button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
