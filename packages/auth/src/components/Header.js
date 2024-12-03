be3d2ad316c392c765709be60fa2ead4412c932c
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-12-03 13:52:41 +0530

    fdad

--- packages/auth/src/components/Header.js
+++ packages/auth/src/components/Header.js
@@ -1,16 +1,7 @@
 import React from "react";
 
 function Header() {
-  return (
-    <div>
-      Header
-      <div></div>
-      <div>
-        <button>login</button>
-        <button>logout</button>
-      </div>
-    </div>
-  );
+  return <div>Header</div>;
 }
 
 export default Header;
